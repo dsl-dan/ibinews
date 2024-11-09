a98b7a4b546fea85e133c49a432c8850ce5aa98b
Dan Lima <150552170+dsl-dan@users.noreply.github.com>
2024-11-09 19:11:12 +0000

    portifolio

--- pages/index.js
+++ pages/index.js
@@ -1,76 +1,18 @@
-import React from "react";
+import database from "infra/database.js";
 
-function Home() {
-  return (
-    <div
-      style={{
-        textAlign: "center",
-        backgroundColor: "#ffe6e6",
-        margin: 0,
-        padding: 0,
-        fontFamily: "Arial, sans-serif",
-        minHeight: "100vh",
-        display: "flex",
-        flexDirection: "column",
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <div style={{ padding: "20px", maxWidth: "600px", width: "100%" }}>
-        <h1 style={{ color: "#ff6666", fontSize: "2rem" }}>
-          Invadir o site só para te desejar: Feliz Dia dos Namorados, Meu Amor!
-        </h1>
-        <div style={{ margin: "20px 0" }}>
-          <img
-            src="https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExNXc4amY1NTJqMXhkeW9xc210MHJ2NXo3NWxkeG15ang3cmE5aWY5biZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/7W1rgKAxlDe3m/giphy.gif"
-            alt="Corações Animados"
-            style={{ maxWidth: "100%", height: "auto" }}
-          />
-        </div>
-        <p style={{ color: "#333", fontSize: "1.2em" }}>
-          <strong>Pri</strong>, minha linda Esposa,
-        </p>
-        <p style={{ color: "#333", fontSize: "1.2em" }}>
-          Neste Dia dos Namorados, quero que você saiba o quanto eu te amo e
-          aprecio cada momento que passamos juntos. Você é a luz da minha vida e
-          a razão do meu sorriso todos os dias. No final de todos os dias, você
-          faz tudo valer a pena!
-        </p>
-        <div style={{ margin: "20px 0" }}>
-          <img
-            src="https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExb3Q0NDdnY3RldDd2ejczeDd3dGdpZjE5b3kyZGVuNGw5dTNuenl1diZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/VIKa3CjZDCoymNcBY5/giphy.gif"
-            alt="Amor e Romance"
-            style={{ maxWidth: "100%", height: "auto" }}
-          />
-        </div>
-        <p style={{ color: "#333", fontSize: "1.2em" }}>
-          Obrigado por ser a parceira incrível que você é. Meu amor por você é
-          indescritível! Quem sabe no futuro isso seja possível de mensurar, mas
-          no momento esse sentimento é indescritível!
-        </p>
-        <div style={{ margin: "20px 0" }}>
-          <img
-            src="https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExZWRtdWw2bjY3dDg5cDlpMTk5MnJnanozem9jZWhncHlkdGluaXNlMSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/LDBZq2hp0Mlrw5Szyj/giphy.gif"
-            alt="Flores para Você"
-            style={{ maxWidth: "100%", height: "auto" }}
-          />
-        </div>
-        <p style={{ color: "#333", fontSize: "1.2em" }}>
-          Com todo o meu amor,
-          <br />
-          <strong>Dani</strong> ;*
-        </p>
-      </div>
-    </div>
-  );
+async function status(request, response) {
+  const updatedAt = new Date().toISOString();
+
+  const databaseVersionResult = await database.query("SHOW server_version;");
+  const databaseVersionValue = databaseVersionResult.rows[0].server_version;
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: databaseVersionValue,
+      },
+    },
+  });
 }
-
-function teste() {
-  console.log("teste");
-}
-
-function teste2() {
-  console.log("identação errada");
-}
-
-export default Home;
+export default status;
